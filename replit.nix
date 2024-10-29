e899164b4a5b355e7ebadf568d22f37830de83e0
Ian Butler <itb206@nyu.edu>
2024-10-29 01:54:38 +0000

    more replit stuff for deploy

--- replit.nix
+++ replit.nix
@@ -1,5 +1,9 @@
-{pkgs}: {
+{ pkgs }: {
+  permittedInsecurePackages = [
+    "openssl-1.1.1w"
+  ];
   deps = [
-    pkgs.postgresql
+    pkgs.openssl_1_1
+    # Add other dependencies as needed
   ];
 }
